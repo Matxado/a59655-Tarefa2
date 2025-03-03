73c26ed2aa47c05a867dffebe71dbaf6d3731d2a
Matxado <a59655@alunos.ipb.pt>
2025-03-03 21:44:26 +0000

    Controller e +ambient

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -13,9 +14,45 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+
+            horizontal = -1.0f;
+
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+
+            horizontal = 1.0f;
+
+        }
+
+        Debug.Log(horizontal);
+
+
+        float vertical = 0.0f;
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+
+            vertical = -1.0f;
+
+        }
+        else if (Keyboard.current.upArrowKey.isPressed)
+        {
+
+            vertical = 1.0f;
+
+        }
+
+        Debug.Log(vertical);
+
+
+
         Vector2 position = transform.position;
-        position.y = position.y + 0.05f;
-        position.x = position.x + 0.05f;
+        position.y = position.y + 0.05f * vertical;
+        position.x = position.x + 0.05f * horizontal;
         transform.position = position;
+        
     }
 }
