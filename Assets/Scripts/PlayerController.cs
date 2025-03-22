53c86322620aaa7e05fc1e30874c191a76b1a2c9
Matxado <a59655@alunos.ipb.pt>
2025-03-22 13:00:49 +0000

    Dynamic Camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,34 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
-
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
     public float speed = 3.0f;
     public int maxHealth = 5;
-    int currentHealth;
     public int health { get { return currentHealth; } }
+    int currentHealth;
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float damageCooldown;
-    public GameObject projectilePrefab;
-
-
     Animator animator;
     Vector2 moveDirection = new Vector2(1, 0);
+    public GameObject projectilePrefab;
 
 
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-
-
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
 
@@ -38,13 +34,11 @@ public class PlayerController : MonoBehaviour
     {
         move = MoveAction.ReadValue<Vector2>();
 
-
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             moveDirection.Set(move.x, move.y);
             moveDirection.Normalize();
         }
-
 
         animator.SetFloat("Look X", moveDirection.x);
         animator.SetFloat("Look Y", moveDirection.y);
@@ -55,14 +49,24 @@ public class PlayerController : MonoBehaviour
         {
             damageCooldown -= Time.deltaTime;
             if (damageCooldown < 0)
+            {
                 isInvincible = false;
+            }
         }
+
 
         if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
         }
+
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            FindFriend();
+        }
     }
+
 
     void FixedUpdate()
     {
@@ -70,20 +74,18 @@ public class PlayerController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-
     public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
             if (isInvincible)
+            {
                 return;
-
+            }
             isInvincible = true;
             damageCooldown = timeInvincible;
             animator.SetTrigger("Hit");
         }
-
-
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
     }
@@ -96,4 +98,16 @@ public class PlayerController : MonoBehaviour
         animator.SetTrigger("Launch");
     }
 
+    void FindFriend()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, moveDirection, 1.5f, LayerMask.GetMask("NPC"));
+        if (hit.collider != null)
+        {
+            NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+            if (character != null)
+            {
+                UIHandler.instance.DisplayDialogue();
+            }
+        }
+    }
 }
